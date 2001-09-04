8e8525f3107626cc6fdf614825d788d815c82a5b
Vadim Zeitlin <vadim@zeitlins.org>
2001-09-04 17:23:59 +0000

    1. added image saving 2. bumped up version to 0.02 3. added icons to the distribution

--- src/wxZoom.cpp
+++ src/wxZoom.cpp
@@ -23,35 +23,44 @@
 #endif
 
 // For compilers that support precompilation, includes "wx/wx.h".
-#include "wx/wxprec.h"
+#include <wx/wxprec.h>
 
 #ifdef __BORLANDC__
     #pragma hdrstop
 #endif
 
 // for all others, include the necessary headers (this file is usually all you
-// need because it includes almost all "standard" wxWindows headers)
+// need because it includes almost all standard wxWindows headers)
 #ifndef WX_PRECOMP
-    #include "wx/intl.h"
-    #include "wx/dynarray.h"
-    #include "wx/timer.h"
+    #include <wx/intl.h>
+    #include <wx/dynarray.h>
+    #include <wx/timer.h>
 
-    #include "wx/app.h"
-    #include "wx/frame.h"
+    #include <wx/app.h>
+    #include <wx/frame.h>
 
-    #include "wx/menu.h"
+    #include <wx/menu.h>
 
-    #include "wx/dcscreen.h"
-    #include "wx/dcmemory.h"
+    #include <wx/filedlg.h>
+    #include <wx/msgdlg.h>
+
+    #include <wx/dcscreen.h>
+    #include <wx/dcmemory.h>
 #endif
 
-#include "wx/image.h"
+#include <wx/image.h>
 
-#include "wx/textdlg.h"
+#include <wx/textdlg.h>
 
 #ifdef __VISUALC__
-#pragma warning(disable: 4355) // this used in base member initializer list
+    #pragma warning(disable: 4355) // this used in base member initializer list
 #endif // VC++
+
+#ifndef __WXMSW__
+    #include "../icons/wxZoom.xpm"
+#endif
+
+#include "config.h"     // for VERSION
 
 // ----------------------------------------------------------------------------
 // constants
@@ -60,14 +69,20 @@
 // menu commands
 enum
 {
-    Zoom_New_Window,
-    Zoom_Quit,
+    Zoom_File_New,
+    Zoom_File_Save_Zoomed,
+    Zoom_File_Save_Unzoomed,
+    Zoom_File_Quit,
+
     Zoom_Zoom_In,
     Zoom_Zoom_Out,
     Zoom_Toggle_Grid,
     Zoom_Toggle_AutoRefresh,
     Zoom_Set_AutoRefreshDelay,
-    Zoom_Refresh
+    Zoom_Refresh,
+
+    Zoom_Help_About,
+    Zoom_Help_Help
 };
 
 // ----------------------------------------------------------------------------
@@ -104,6 +119,7 @@ public:
     void OnMouseDown(wxMouseEvent& event);
     void OnMouseMove(wxMouseEvent& event);
     void OnMouseUp(wxMouseEvent& event);
+    void OnKeyDown(wxKeyEvent& event);
     void OnTimer(wxTimerEvent& event);
     void OnUpdateZoomOut(wxUpdateUIEvent& event);
 
@@ -115,6 +131,16 @@ protected:
 
     void DrawMarker();
     void DrawMarker(wxDC& dc, const wxPoint& pt);
+
+    // zoom the image to have the size of this frame
+    void ZoomImage(wxImage& image);
+
+    // stop dragging the selection rect: erase the marker and reset the flag
+    void EndDragging();
+
+    // get the real mouse position on screen from coords provided by
+    // wxMouseEvent
+    wxPoint PtMouse2Screen(const wxMouseEvent& event);
 
     bool m_captured;
     wxPoint m_oldPt;
@@ -135,12 +161,8 @@ protected:
 };
 
 // ============================================================================
-// implementation
+// ZoomApp implementation
 // ============================================================================
-
-// ----------------------------------------------------------------------------
-// ZoomApp
-// ----------------------------------------------------------------------------
 
 IMPLEMENT_APP(ZoomApp)
 
@@ -162,9 +184,9 @@ void ZoomApp::CloseAll()
     }
 }
 
-// ----------------------------------------------------------------------------
-// ZoomFrame
-// ----------------------------------------------------------------------------
+// ============================================================================
+// ZoomFrame implementation
+// ============================================================================
 
 BEGIN_EVENT_TABLE(ZoomFrame, wxFrame)
     EVT_PAINT(ZoomFrame::OnPaint)
@@ -175,11 +197,17 @@ BEGIN_EVENT_TABLE(ZoomFrame, wxFrame)
     EVT_MOTION(ZoomFrame::OnMouseMove)
     EVT_LEFT_UP(ZoomFrame::OnMouseUp)
 
+    EVT_KEY_DOWN(ZoomFrame::OnKeyDown)
+
     EVT_MENU(-1, ZoomFrame::OnCommand)
     EVT_UPDATE_UI(Zoom_Zoom_Out, ZoomFrame::OnUpdateZoomOut)
 
     EVT_TIMER(-1, ZoomFrame::OnTimer)
 END_EVENT_TABLE()
+
+// ----------------------------------------------------------------------------
+// ZoomFrame ctor
+// ----------------------------------------------------------------------------
 
 ZoomFrame::ZoomFrame(const wxSize& size, const wxPoint& ptOld)
          : wxFrame(NULL, -1, _("wxZoom"), wxDefaultPosition, size),
@@ -194,9 +222,13 @@ ZoomFrame::ZoomFrame(const wxSize& size, const wxPoint& ptOld)
 
     wxMenuBar *menuBar = new wxMenuBar;
     wxMenu *menuFile = new wxMenu("", wxMENU_TEAROFF);
-    menuFile->Append(Zoom_New_Window, _("&New window\tCtrl-N"));
+    menuFile->Append(Zoom_File_New, _("&New window\tCtrl-N"));
     menuFile->AppendSeparator();
-    menuFile->Append(Zoom_Quit, _("E&xit\tCtrl-Q"));
+    menuFile->Append(Zoom_File_Save_Unzoomed, _("&Save image...\tCtrl-S"));
+    menuFile->Append(Zoom_File_Save_Zoomed,
+                     _("Save &zoomed image...\tAlt-Ctrl-S"));
+    menuFile->AppendSeparator();
+    menuFile->Append(Zoom_File_Quit, _("E&xit\tCtrl-Q"));
     menuBar->Append(menuFile, _("&File"));
 
     wxMenu *menuView = new wxMenu("", wxMENU_TEAROFF);
@@ -210,12 +242,30 @@ ZoomFrame::ZoomFrame(const wxSize& size, const wxPoint& ptOld)
     menuView->AppendSeparator();
     menuView->Append(Zoom_Toggle_Grid, _("Toggle &grid\tCtrl-G"), _T(""), TRUE);
     menuBar->Append(menuView, _("&View"));
+
+    wxMenu *menuHelp = new wxMenu;
+    menuHelp->Append(Zoom_Help_About, _T("&About..."));
+    menuHelp->AppendSeparator();
+    menuHelp->Append(Zoom_Help_Help, _T("&Help...\tF1"));
+    menuBar->Append(menuHelp, _("&Help"));
+
     SetMenuBar(menuBar);
 
     CreateStatusBar(3);
+    static const int widths[3] =
+    {
+        -1,
+        100,
+        100
+    };
+    SetStatusWidths(3, widths);
 
     SetIcon(wxICON(wxZoom));
 }
+
+// ----------------------------------------------------------------------------
+// ZoomFrame helper methods
+// ----------------------------------------------------------------------------
 
 void ZoomFrame::DrawMarker(wxDC& dc, const wxPoint& pt)
 {
@@ -242,6 +292,11 @@ void ZoomFrame::RecreateBitmap()
     RefreshBitmap();
 }
 
+void ZoomFrame::ZoomImage(wxImage& image)
+{
+    image.Rescale(m_zoomFactor*m_sizeBmp.x, m_zoomFactor*m_sizeBmp.y);
+}
+
 void ZoomFrame::RefreshBitmap()
 {
     wxScreenDC dcScreen;
@@ -263,9 +318,28 @@ void ZoomFrame::RestartTimer()
         m_timer.Start(m_refreshTime);
 }
 
+void ZoomFrame::EndDragging()
+{
+    // erase it, in fact
+    DrawMarker();
+
+    ReleaseMouse();
+
+    m_captured = FALSE;
+}
+
+wxPoint ZoomFrame::PtMouse2Screen(const wxMouseEvent& event)
+{
+    return event.GetPosition() + GetPosition();
+}
+
+// ----------------------------------------------------------------------------
+// ZoomFrame event handlers
+// ----------------------------------------------------------------------------
+
 void ZoomFrame::OnMouseDown(wxMouseEvent& event)
 {
-    m_oldPt = event.GetPosition() + GetPosition();
+    m_oldPt = PtMouse2Screen(event);
 
     CaptureMouse();
     DrawMarker();
@@ -275,12 +349,11 @@ void ZoomFrame::OnMouseDown(wxMouseEvent& event)
 
 void ZoomFrame::OnMouseMove(wxMouseEvent& event)
 {
-    wxPoint pt = event.GetPosition();
-
     if ( m_captured )
     {
+        // continue dragging the rectangle on screen
         wxScreenDC dc;
-        pt += GetPosition();
+        wxPoint pt = PtMouse2Screen(event);
 
         if ( pt != m_oldPt )
         {
@@ -307,11 +380,11 @@ void ZoomFrame::OnMouseMove(wxMouseEvent& event)
             dc.EndDrawingOnTop();
         }
     }
-    else // not captured
+    else // not captured: show the colour of the pixel under mouse
     {
         wxClientDC dc(this);
         wxColour col;
-        dc.GetPixel(pt, &col);
+        dc.GetPixel(event.GetPosition(), &col);
 
         if ( col != m_oldCol )
         {
@@ -327,14 +400,64 @@ void ZoomFrame::OnMouseMove(wxMouseEvent& event)
 
 void ZoomFrame::OnMouseUp(wxMouseEvent& event)
 {
-    // erase it, in fact
-    DrawMarker();
+    EndDragging();
 
-    ReleaseMouse();
-
+    // show the selected area
     RefreshBitmap();
+}
 
-    m_captured = FALSE;
+void ZoomFrame::OnKeyDown(wxKeyEvent& event)
+{
+    // we only handle events while dragging the rect on screen
+    if ( m_captured )
+    {
+        wxCoord dx = 0,
+                dy = 0;
+
+        switch ( event.GetKeyCode() )
+        {
+            case WXK_RIGHT:
+                dx = 1;
+                break;
+
+            case WXK_LEFT:
+                dx = -1;
+                break;
+
+            case WXK_UP:
+                dy = -1;
+                break;
+
+            case WXK_DOWN:
+                dy = 1;
+                break;
+
+            case WXK_ESCAPE:
+                EndDragging();
+                return;
+
+            default:
+                event.Skip();
+                return;
+        }
+
+        if ( event.ControlDown() )
+        {
+            dx *= 10;
+            dy *= 10;
+        }
+
+        if ( event.AltDown() || event.MetaDown() )
+        {
+            dx *= 50;
+            dy *= 50;
+        }
+
+        wxPoint pt = event.GetPosition();
+        pt.x += dx;
+        pt.y += dy;
+        WarpPointer(pt.x, pt.y);
+    }
 }
 
 void ZoomFrame::OnSize(wxSizeEvent& event)
@@ -355,9 +478,9 @@ void ZoomFrame::OnPaint(wxPaintEvent& event)
     }
     else
     {
-        bmp = wxImage(m_bmp).
-                Rescale(m_zoomFactor*m_sizeBmp.x, m_zoomFactor*m_sizeBmp.y).
-                    ConvertToBitmap();
+        wxImage img = m_bmp;
+        ZoomImage(img);
+        bmp = img.ConvertToBitmap();
     }
 
     dc.DrawBitmap(bmp, 0, 0);
@@ -382,14 +505,75 @@ void ZoomFrame::OnCommand(wxCommandEvent& event)
 {
     switch ( event.GetId() )
     {
-        case Zoom_New_Window:
+        case Zoom_File_New:
             {
                 wxFrame *frame = new ZoomFrame(GetSize(), m_oldPt);
                 frame->Show();
             }
             break;
 
-        case Zoom_Quit:
+        case Zoom_File_Save_Zoomed:
+        case Zoom_File_Save_Unzoomed:
+            {
+                bool zoomed = event.GetId() == Zoom_File_Save_Zoomed;
+
+                wxString what = zoomed ? _("zoomed image")
+                                       : _("original image");
+                wxString filename = wxSaveFileSelector
+                                    (
+                                      what,
+                                      _("BMP files (*.bmp)|*.bmp|"
+                                      "PNG files (*.png)|*.png|"
+                                      "JPEG files (*.jpg)|*.jpg|"
+                                      "GIF files (*.gif)|*.gif|"
+                                      "TIFF files (*.tif)|*.tif|"
+                                      "PCX files (*.pcx)|*.pcx"),
+                                      _T(""),       // no default name
+                                      this
+                                    );
+                if ( filename.empty() )
+                {
+                    // cancelled
+                    break;
+                }
+
+                wxImage img = m_bmp;
+                if ( zoomed )
+                {
+                    ZoomImage(img);
+                }
+
+                wxString ext;
+                wxSplitPath(filename, NULL, NULL, &ext);
+                ext.MakeLower();
+
+                int type;
+                if ( ext == _T("png") )
+                    type = wxBITMAP_TYPE_PNG;
+                else if ( ext == _T("jpg") || ext == _T("jpeg") )
+                    type = wxBITMAP_TYPE_JPEG;
+                else if ( ext == _T("pcx") )
+                    type = wxBITMAP_TYPE_PCX;
+                else if ( ext == _T("tif") || ext == _T("tiff") )
+                    type = wxBITMAP_TYPE_TIF;
+                else if ( ext == _T("pnm") )
+                    type = wxBITMAP_TYPE_PNM;
+                else // bmp by default
+                    type = wxBITMAP_TYPE_BMP;
+
+                if ( !img.SaveFile(filename, type) )
+                {
+                    wxLogError(_("Failed to save the image file."));
+                }
+                else
+                {
+                    wxLogStatus(this, _("Saved %s to '%s'."),
+                                what.c_str(), filename.c_str());
+                }
+            }
+            break;
+
+        case Zoom_File_Quit:
             wxGetApp().CloseAll();
             break;
 
@@ -448,6 +632,31 @@ void ZoomFrame::OnCommand(wxCommandEvent& event)
 
         case Zoom_Refresh:
             RefreshBitmap();
+            break;
+
+        case Zoom_Help_About:
+            wxMessageBox(_("wxZoom " VERSION
+                                   ": cross-platform display magnifier.\n"
+                           "\n"
+                           "This is free software, enjoy using it!\n"
+                           "\n"
+                           "(c) 2000-2001 Vadim Zeitlin"),
+                         _("About wxZoom"),
+                         wxICON_INFORMATION | wxOK, this);
+            break;
+
+        case Zoom_Help_Help:
+            wxMessageBox(_("Press the left mouse button in the window "
+                           "and drag the selection rectangle to select "
+                           "the screen area to zoom.\n"
+                           "\n"
+                           "You may change the zoom setting using the "
+                           "commands from the \"View\" menu.\n"
+                           "\n"
+                           "Please send any questions/suggestions to "
+                           "vadim@wxwindows.org"),
+                         _("wxZoom Help"),
+                         wxICON_INFORMATION | wxOK, this);
             break;
 
         default:
